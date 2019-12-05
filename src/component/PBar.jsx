6dd6655e1ddf08d69d47dadbeea51acb0ae8730c
Aurélien <aurelien.llorca@gmail.com>
2019-12-05 21:04:55 +0100

    progress bar wip

--- src/component/PBar.jsx
+++ src/component/PBar.jsx
@@ -1,15 +1,30 @@
-class PBar extends Component {
-    constructor(props) {
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import React from 'react';
+
+class PBar extends React.Component {
+    constructor(props){
         super(props);
-        this.state = {  }
+        this.setState({
+            critical: false
+        });
     }
+
+    isCritical(props){
+        let diff = props.max - props.min;
+        let sMin = props.min + diff * 1.15;
+        let sMax = props.max - diff * 1.15;
+        this.setState({
+            critical : (props.actual > sMin && props.actual < sMax) ? 
+                true : false});
+
+    }
+
     render() { 
-        return ( 
-            <div>
-                
-            </div>
-         );
+        return (
+            <ProgressBar />
+        );
     }
 }
+ 
  
 export default PBar;
