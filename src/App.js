51b070565d250777ce7709a13ce61e9c70b71a1e
lenaick <lenaick.sorimoutou@gmail.com>
2019-12-05 19:54:37 +0100

    add link bootstrap stylesheet

--- src/App.js
+++ src/App.js
@@ -31,9 +31,15 @@ class App extends React.Component{
   render(){
     return (
       <div className="App">
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossOrigin="anonymous"
+        />
         <BrowserRouter>
           <Switch>
-            <Route exact path="/" component={Home} />
+            <Route exact path="/" component={Home} data={this.state.data} />
             <Route path="*" component={ErrorNotFound} />
           </Switch>
         </BrowserRouter>
