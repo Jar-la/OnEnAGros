ec80a92e7c216fad8be1d75ff3c17ad4748e033d
Alexandre <Alexandre.lafon@outlook.fr>
2019-12-05 21:11:32 +0100

    Added News component

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import News from "./News";
+
 class Header extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <News />
+      </div>
+    );
   }
 }
 
---
+++ src/component/News.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class News extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div className='News'></div>;
+  }
+}
+
+export default News;
