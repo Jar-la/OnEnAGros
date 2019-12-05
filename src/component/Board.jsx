1e8603a54dfd6895ce2236924a21100440780f78
Alexandre <Alexandre.lafon@outlook.fr>
2019-12-05 20:52:22 +0100

    erreur

--- src/component/Board.jsx
+++ src/component/Board.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Component } from "react";
-import { Header } from "./Header";
-import { Center } from "./Center";
-import { Sidebar } from "./Sidebar";
+import Component from "react";
+import Header from "./Header";
+import Center from "./Center";
+import Sidebar from "./Sidebar";
 
 
 class Board extends React.Component {
