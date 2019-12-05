d39c1ba3359c6bc53230e703ca9b4b62a1675a86
lenaick <lenaick.sorimoutou@gmail.com>
2019-12-05 22:00:08 +0100

    update App.js and handle the fetch issue

--- src/component/Center.jsx
+++ src/component/Center.jsx
@@ -13,7 +13,7 @@ class Center extends React.Component {
       <div className="Center">
         <Date data={this.props.data} />
         <Alert data={this.props.data} />
-        <Control data={this.props.data} />
+        <Control />
       </div>
     );
   }
