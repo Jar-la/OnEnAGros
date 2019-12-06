4c5765e99442c387f97cc6ef719cbfcc183ef5a6
Alexandre <Alexandre.lafon@outlook.fr>
2019-12-06 07:02:58 +0100

    removed bug

--- src/component/SidebarL.jsx
+++ src/component/SidebarL.jsx
@@ -1,6 +1,4 @@
 import DataW from "./DataW";
-import DataA from "./DataA";
-import DataF from "./DataF";
 import React from "react";
 
 class SidebarL extends React.Component {
@@ -15,7 +13,7 @@ class SidebarL extends React.Component {
         <DataW id="Fiabilité de l'air" data={this.props.data.air.dioxygene} />
         <DataW
           id='Fiabilité de la nourriture'
-          data={this.props.data.food["escherichia coli"]}
+          data={this.props.data.food["listeria monocytogenes"]}
         />
       </div>
     );
