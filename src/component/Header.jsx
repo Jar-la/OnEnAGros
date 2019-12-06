af0d009c5a32585c3a36af7d6373bfd1fe7179c5
Alexandre <Alexandre.lafon@outlook.fr>
2019-12-06 01:05:01 +0100

    added badge component

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import News from "./News";
+import Badge from "./Badge";
 
 class Header extends React.Component {
   constructor(props) {
@@ -10,6 +11,7 @@ class Header extends React.Component {
     return (
       <div className="Header">
         Header
+        <Badge />
         <News />
       </div>
     );
