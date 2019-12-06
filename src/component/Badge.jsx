13772ddde8ae9fb2dd9d0ab472010ddb63ffa26a
Aurélien <aurelien.llorca@gmail.com>
2019-12-06 02:29:52 +0100

    fdsfsdf

--- src/component/Badge.jsx
+++ src/component/Badge.jsx
@@ -1,18 +1,8 @@
 import React from "react";
 
 const Badge = () => (
-    <div className="text">
-        <span>S</span>
-        <span>e</span>
-        <span>l</span>
-        <span>e</span>
-        <span>n</span>
-        <span>e</span>
-        <span></span>
-        <span>2</span>
-        <span>0</span>
-        <span>1</span>
-        <span>9</span>
+    <div id ="Logo" className="tracking-in-expand">
+        {"Selene2019"}
     </div>
 )
  
