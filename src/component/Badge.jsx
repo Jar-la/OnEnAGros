52965d6fcf4351815cbe08f894d3bde3606f72b3
Aurélien <aurelien.llorca@gmail.com>
2019-12-06 01:19:27 +0100

    ghh

--- src/component/Badge.jsx
+++ src/component/Badge.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 
-class Badge extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( 
-            <div className="badge">
-                
-            </div>
-         );
-    }
-}
+const Badge = () => (
+    <div className="text">
+        <span>S</span>
+        <span>e</span>
+        <span>l</span>
+        <span>e</span>
+        <span>n</span>
+        <span>e</span>
+        <span></span>
+        <span>2</span>
+        <span>0</span>
+        <span>1</span>
+        <span>9</span>
+    </div>
+)
  
 export default Badge;
